6219ca420eb1d1c50c9043d1f7cf4e68ae697e7f
Edil-Joey2 <77836584+Edil-Joey@users.noreply.github.com>
2023-01-13 19:00:13 +0530

    Add files via upload

---
+++ Restaurent_Website/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <title>Thankyou</title>
+    <style>
+h1{
+    text-align:center;
+    font-size:56px;
+    font-weight:bold;
+    margin:10px;
+}
+    </style>
+    <body>
+        <?php 
+       echo "<h1>Sucessfully Registered </h1>";
+       echo "<h1>Thank you!</h1>";
+        
+        ?>
+    </body>
+</html>
